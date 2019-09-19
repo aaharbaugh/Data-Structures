11d65d6328bcff5089effb50058fe1f5088fc6b3
aaharbaugh <aaronmharbaugh@gmail.com>
2019-09-19 16:36:46 -0500

    finished BST stack queue depth/breadth searches

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -63,12 +63,80 @@ class BinarySearchTree:
     current = self
     
     cb(current.value)
-    
+
     if current.left is not None: 
       current.left.for_each(cb)
     if current.right is not None:
       current.right.for_each(cb)
 
-    
+# Print all the values in order from low to high
+  # Hint:  Use a recursive, depth first traversal
+  def in_order_print(self, node):
+      #check left
+      #if node on left, recursively go left
+      if node.left:
+        node.in_order_print(node.left)
+      
+      #if none left
+      print(node.value)
 
-    
+      if node.right:
+        # recursively go right
+        node.in_order_print(node.right)
+      
+
+  # Print the value of every node, starting with the given node,
+  # in an iterative breadth first traversal
+  def bft_print(self, node):
+      
+      #initialize queue
+      queue = Queue() 
+      #add current node to queue
+      queue.enqueue(node)
+
+      #while queue is not empty
+      while queue.size > 0:
+        #visit top node in queue
+        #remove from queue
+        current = queue.dequeue()
+        #print and
+        print(current.value)
+        #push children to queue
+        if current.left:
+          queue.enqueue(current.left)
+        if current.right:
+          queue.enqueue(current.right)
+
+
+  # Print the value of every node, starting with the given node,
+  # in an iterative depth first traversal
+  def dft_print(self, node):
+            
+      #initialize stack
+      stack = Stack() 
+      #add current node to stack
+      stack.push(node)
+
+      #while stack is not empty
+      while stack.size > 0:
+        #visit top node in stack
+        #remove from stack
+        current = stack.pop()
+        #print and
+        print(current.value)
+        #push children to stack
+        if current.left:
+          stack.push(current.left)
+        if current.right:
+          stack.push(current.right)
+
+  # STRETCH Goals -------------------------
+  # Note: Research may be required
+
+  # Print In-order recursive DFT
+  def pre_order_dft(self, node):
+      pass
+
+  # Print Post-order recursive DFT
+  def post_order_dft(self, node):
+      pass
